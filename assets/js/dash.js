eb47d55aceb45e38c17df9eae739a3aba8930748
shina1 <oswasiu@gmail.com>
2020-08-09 22:28:23 +0200

    profile page

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-  //   var adjustSidebar = function () {
-  //     $(".sidebar").slimScroll({
-  //       height: (document.documentElement.clientHeight = $(
-  //         ".navbar"
-  //       ).outerHeight()),
-  //     });
-  //   };
-  //   adjustSidebar();
   $(".sideMenuToggler").on("click", function () {
     $(".wrapper").toggleClass("active");
   });
